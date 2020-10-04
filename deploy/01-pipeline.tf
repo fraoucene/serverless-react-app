cd610fba27ba0f1d6c50508d3cd1fe85b5ca457c
Fraoucene <fraoucene.guezout@gmail.com>
2020-10-04 22:11:44 +0200

    add dev branch

--- deploy/01-pipeline.tf
+++ deploy/01-pipeline.tf
@@ -62,7 +62,7 @@ resource "aws_codepipeline" "react-serverless-codepipeline" {
       configuration = {
         Owner      = var.github_username
         Repo       = var.github_project_name
-        Branch     = "master"
+        Branch     = "dev"
         OAuthToken = var.github_token
       }
     }
